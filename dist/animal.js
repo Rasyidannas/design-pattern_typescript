c665888bb26d91177a31321f164d1263b5056dc2
rasyidannas <rasshit.dsgn@gmail.com>
2025-04-12 12:58:34 +0700

    2/1. Getting Started.mp4

--- dist/animal.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-}
-exports.default = Animal;
