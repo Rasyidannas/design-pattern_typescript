d412700d7bc6e6ae2e6b8d254d01906aca20ddc0
rasyidannas <rasshit.dsgn@gmail.com>
2025-04-13 17:57:38 +0700

    2/16. Access Modifiers.mp4

--- src/test.ts
+++ src/test.ts
@@ -1,53 +1,19 @@
-abstract class Animal {
-    abstract name: string
-    age = -1
+class Animal {
+    protected name: string
+    protected age: number
 
-    constructor() {}
-
-    abstract feed(food: string, amount: number): void
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
 }
 
 class Cat extends Animal {
-    name: string
     constructor(name: string, age: number) {
-        super()
-        this.name = name
-        this.age = age
-    }
-
-    feed(food: string, amount: number): void {
-        console.log(
-            'Feeding ' +
-                this.name +
-                ' the Cat ' +
-                amount +
-                ' kg of ' +
-                food
-        )
-    }
-}
-
-class Dog extends Animal {
-    name: string
-    constructor(name: string, age: number) {
-        super()
-        this.name = name
-        this.age = age
-    }
-
-    feed(food: string, amount: number): void {
-        console.log(
-            'Feeding ' +
-                this.name +
-                ' the Dog ' +
-                amount +
-                ' kg of ' +
-                food
-        )
+        super(name, age)
+        console.log(this.name)
     }
 }
 
 const CAT = new Cat('Cosmo', 8)
-const DOG = new Dog('Rusty', 12)
-CAT.feed('Fish', 0.1)
-DOG.feed('Beef', 0.25)
+console.log(CAT)
