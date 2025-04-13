8161e4a41cb617c399a3c432c681d023ca63f921
rasyidannas <rasshit.dsgn@gmail.com>
2025-04-13 18:17:17 +0700

    2/17. Static Members.mp4

--- dist/test.js
+++ dist/test.js
@@ -1,15 +1,14 @@
 "use strict";
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
     }
 }
-class Cat extends Animal {
-    constructor(name, age) {
-        super(name, age);
-        console.log(this.name);
-    }
-}
-const CAT = new Cat('Cosmo', 8);
-console.log(CAT);
+Circle.PI = 3.14;
+console.log('Circle.PI = ' + Circle.PI);
+const CIRCLE1 = new Circle(1);
+const CIRCLE2 = new Circle(2);
+const CIRCLE3 = new Circle(3);
+console.log('CIRCLE1 Area = ' + Circle.PI * Math.pow(CIRCLE1.radius, 2));
+console.log('CIRCLE2 Area = ' + Circle.PI * Math.pow(CIRCLE2.radius, 2));
+console.log('CIRCLE3 Area = ' + Circle.PI * Math.pow(CIRCLE3.radius, 2));
--- src/test.ts
+++ src/test.ts
@@ -1,19 +1,17 @@
-class Animal {
-    protected name: string
-    protected age: number
+class Circle {
+    radius: number
+    static PI = 3.14
 
-    constructor(name: string, age: number) {
-        this.name = name
-        this.age = age
+    constructor(radius: number) {
+        this.radius = radius
     }
 }
 
-class Cat extends Animal {
-    constructor(name: string, age: number) {
-        super(name, age)
-        console.log(this.name)
-    }
-}
+console.log('Circle.PI = ' + Circle.PI)
 
-const CAT = new Cat('Cosmo', 8)
-console.log(CAT)
+const CIRCLE1 = new Circle(1)
+const CIRCLE2 = new Circle(2)
+const CIRCLE3 = new Circle(3)
+console.log('CIRCLE1 Area = ' + Circle.PI * CIRCLE1.radius ** 2)
+console.log('CIRCLE2 Area = ' + Circle.PI * CIRCLE2.radius ** 2)
+console.log('CIRCLE3 Area = ' + Circle.PI * CIRCLE3.radius ** 2)
